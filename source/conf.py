f9577d28853658233075874329e4a48406a63039
feiyanke <feiyanke@gmail.com>
2018-05-07 15:38:40 +0800

    draft

--- source/conf.py
+++ source/conf.py
@@ -85,7 +85,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
+html_theme_path = ["_theme", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- source/index.rst
+++ source/index.rst
@@ -18,3 +18,8 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
+.. note::
+    12324556
+
+
